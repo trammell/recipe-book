3161917a750071f7a62fd28170ef1d9f9a162943
John Trammell <john.trammell@gmail.com>
2023-04-27 17:56:40 -0500

    add github link

--- pelicanconf.py
+++ pelicanconf.py
@@ -16,14 +16,10 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('Source code on GitHub', 'https://github.com/trammell/recipe-book'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = ()
 
 DEFAULT_PAGINATION = False
 
